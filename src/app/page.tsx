256b88ddc2fa9aa91a36a37bf2c44d00ede47dd4
manojk765 <manojkoneti05@gmail.com>
2024-09-22 18:14:45 +0530

    Small Changes in imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import NavBar from '../components/NavBar.jsx';
 import Homepage from '../components/Homepage.jsx';
-import About from './about/page.jsx';
-import Testimonials from './about/Testimonials.jsx';
-import Services from './services/page.jsx';
-import Doctors from '../components/Doctors.jsx';
-import Footer from '../components/Footer.jsx'
-import ContactUs from './contact/page.jsx'
+
 
 export default function Home() {
   return (
